058761554e6a4e61ef8e1f82fc91266500fb78cb
Dave Homrighouse <128435720+davehomrighouse@users.noreply.github.com>
2026-08-25 14:43:16 -0700

    Change default server port to 10000

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ with gr.Blocks() as demo:
 
 if __name__ == "__main__":
     server_name="0.0.0.0",
-    server_port=int(os.environ.get("PORT", 7860))
+    server_port=int(os.environ.get("PORT", 10000))
