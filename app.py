044a3e1db16c2cbc296c69c1a463284e29c9ec3d
Dave Homrighouse <128435720+davehomrighouse@users.noreply.github.com>
2026-08-25 15:54:34 -0700

    Fix server launch parameters in app.py

--- app.py
+++ app.py
@@ -71,5 +71,9 @@ with gr.Blocks() as demo:
     )
 
 if __name__ == "__main__":
-    server_name="0.0.0.0",
-    server_port=int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))
+
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=port
+    )
